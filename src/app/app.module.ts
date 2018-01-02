b5c5cbd8653147454bc14796e27b016de1ae562a
MNakel <MNakel@SISFirst.com>
2018-01-02 11:56:20 -0500

    Created two new components, and added routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'; // <-- here
+import { RoundProgressModule } from 'angular-svg-round-progressbar'; // <-- here
+// import { MatButtonModule, MatCheckboxModule, MatToolbar, MatMenu } from '@angular/material';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { TimerComponent } from './primarySectors/timer/timer.component';
+import { LandingPageComponent } from './primarySectors/landing-page/landing-page.component';
+
+const appRoutes: Routes = [
+  { path: "", redirectTo: "/dashboard", pathMatch: "full" },
+  { path: "dashboard", component: LandingPageComponent },
+  { path: "timer", component: TimerComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimerComponent,
+    LandingPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),    
+    FormsModule,
+    RoundProgressModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/primarySectors/timer/timer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'desk-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.css']
+})
+export class TimerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

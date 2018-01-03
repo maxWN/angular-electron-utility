ca58843d45ec9260dc204633e575377ed24620c8
MNakel <MNakel@SISFirst.com>
2018-01-03 10:09:07 -0500

    Modified soundWidget and added music files.

--- src/app/primarySectors/sound-widget/sound-widget.component.ts
+++ src/app/primarySectors/sound-widget/sound-widget.component.ts
@@ -10,10 +10,30 @@ import { Playlist } from '../../dataModels/playlist';
 export class SoundWidgetComponent implements OnInit {
 
   public AudioFiles: Playlist;
+  //song title model
+  public songTitle:string = "Track";
 
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.AudioFiles.song = new Howl({
+      src:["assets/sounds/Maid with the Flaxen Hair.mp3"],
+      volume:.25,
+      onend:() => { alert("\"Maid with the Flaxen Hair\" has finished.") }
+    });
+  }
+
+  public stopSong():void {
+    this.AudioFiles.song.play();
+  }
+
+  public playSong():void {
+    if( this.AudioFiles.song.duration > 0) {
+      this.AudioFiles.song.stop();
+    }
+
   }
 
 }

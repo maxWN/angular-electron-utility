185e70c058859b3269273a865de831cb8320e1e2
MNakel <MNakel@SISFirst.com>
2018-01-19 17:08:31 -0500

    Modified drop-down display functionality.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,7 +37,7 @@ export class AppComponent {
   }
 
   openDropdown():void {
-    this.ifClicked=true;
+    this.ifClicked=!this.ifClicked;
     this.interval = setInterval(this.vaporize(), 8000);
   }
 

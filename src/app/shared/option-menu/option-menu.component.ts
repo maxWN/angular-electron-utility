6a621827f3d89550ebaf08db50076957bc6c9264
MNakel <MNakel@SISFirst.com>
2018-01-19 17:10:03 -0500

    Created form control functionality for music search in option-menu component.

--- src/app/shared/option-menu/option-menu.component.ts
+++ src/app/shared/option-menu/option-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Event } from 'electron';
 
 
 @Component({
@@ -9,8 +10,11 @@ import { Component, OnInit } from '@angular/core';
 export class OptionMenuComponent implements OnInit {
 
   isSearchDisplayed:boolean=false;
+  songTitle:string="Upload one or more files";
 
   constructor() { }
+
+  //region public functions
 
   public ngOnInit():void {
   }
@@ -18,5 +22,32 @@ export class OptionMenuComponent implements OnInit {
   public displaySearch():void {
     this.isSearchDisplayed=!this.isSearchDisplayed;
   }
+
+  public onHandleUpload($event): void {
+    if($event) {
+      let x:File = $event.target.files[0];
+      alert("name"+x.name);
+    }
+  }
+
+  //endregion public functions
+
+  //region private functions
+
+  //determine cause of error within 
+  private readURL(input):void {
+
+    // if (input.files && input.files[0]) {
+      let reader = new FileReader();
+  
+      reader.onloadend = function(e) {
+        //insert service call
+      }
+      // this.songTitle=String(input.files[0]);
+      reader.readAsDataURL(input.files[0]);
+    // }
+  }
+
+  //endregion private functions
 
 }

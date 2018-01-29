44a8511e8f4fb84d1fb38358bb98a03179206104
Max <maxnakel@gmail.com>
2018-01-28 19:27:45 -0500

    Added zoom-in-out feature to interactive-map component.

--- src/app/primarySectors/interactive-map/interactive-map.component.ts
+++ src/app/primarySectors/interactive-map/interactive-map.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import * as ol from 'openlayers';
-// import { layer, render, Object, View, control, Overlay, Map } as ol from 'C:/Users/mnakel.SISFIRST/Documents/Development Files/LOCAL_PROJECTS/angular-desktop/node_modules/@types/openlayers';
 
 @Component({
   selector: 'desk-interactive-map',
@@ -9,38 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InteractiveMapComponent implements OnInit {
 
+  //region class variables 
+  
+    public zoom: number;
+
+  //endregion class variables 
+
   constructor() { }
 
   ngOnInit() {
-    // let map = new Map({
-    //   layers: [
-    //     new layer.Tile({
-    //       source: new ol.source.OSM()
-    //     })
-    //   ],
-    //   target: 'map',
-    //   controls: control.defaults({
-    //     attributionOptions: {
-    //       collapsible: false
-    //     }
-    //   }),
-    //   view: new View({
-    //     center: [0, 0],
-    //     zoom: 2
-    //   })
-    // });
-
-    // document.getElementById('zoom-out').onclick = function() {
-    //   let view = map.getView();
-    //   let zoom = view.getZoom();
-    //   view.setZoom(zoom - 1);
-    // };
-
-    // document.getElementById('zoom-in').onclick = function() {
-    //   let view = map.getView();
-    //   let zoom = view.getZoom();
-    //   view.setZoom(zoom + 1);
-    // };
+    this.zoom = 1;
   }
 
 }

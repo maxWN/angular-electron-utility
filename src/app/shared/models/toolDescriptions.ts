6805d1a6afef7667c3efdda45f57ee0baf570f90
Max <maxnakel@gmail.com>
2018-02-19 00:35:16 -0500

    Modified tool description model file and added two new classes.

--- src/app/shared/models/toolDescriptions.ts
+++ src/app/shared/models/toolDescriptions.ts
@@ -1,14 +1,40 @@
 
-export class ToolDescriptions {
+/**
+ * ToolDescriptionsUS class represents English language descriptions
+ * of each widget within the application
+ * @property titles {string} represents widget descriptions
+ **/
+export class ToolDescriptionsUS {
 
     public MUSIC_PLAYER: string = "The music player application allows you to "+
     "search files on your computer, play any type of file, edit the files,"+
     " play a background animation that syncs with the music, and have a personal "+
     "library of all of your favorite tunes.";
-    public FILE_MANAGER: string = "1";
-    public VIDEO_PLAYER: string = "2";
-    public SOUND_WIDGET: string = "3";
-    public INTERACTIVE_MAP: string = "4";
-    public TEXT_EDITOR: string = "5";
+    public FILE_MANAGER: string = "The File manager widget allows you to "+
+    "search within your computers folders, rename files, add files, and delete files.";
+    public VIDEO_PLAYER: string = "The video player widget allows you to search for video files" + 
+    "on your computer, play any type of file, edit the files, and have a personal library of all your favorite videos.";
+    public INTERACTIVE_MAP: string = "The interactive map widget allows users to search for locations all across the globe.";
+    public TEXT_EDITOR: string = "The text editor widget allows users to modify or create text files on their computer (files "+
+    " will be saved as rich text format on Windows)";
             
+}
+
+/**
+ * ToolDescriptionsDE class represents German language descriptions
+ * of each widget within the application
+ * @property titles {string} represents widget descriptions
+ **/
+export class ToolDescriptionsDE {
+    //insert German language descriptions
+} 
+
+
+/**
+ * ToolDescriptionsES class represents Spanish language descriptions
+ * of each widget within the application
+ * @property titles {string} represents widget descriptions
+ **/
+export class ToolDescriptionsES {
+    //insert Spanish language descriptions
 }

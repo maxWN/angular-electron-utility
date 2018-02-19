be2dfe55f32218feccc539a1f7bc72b586978163
Max <maxnakel@gmail.com>
2018-02-18 23:59:07 -0500

    Created model class for landing page tool descriptions.

---
+++ src/app/shared/models/toolDescriptions.ts
@@ -0,0 +1,14 @@
+
+export class ToolDescriptions {
+
+    public MUSIC_PLAYER: string = "The music player application allows you to "+
+    "search files on your computer, play any type of file, edit the files,"+
+    " play a background animation that syncs with the music, and have a personal "+
+    "library of all of your favorite tunes.";
+    public FILE_MANAGER: string = "1";
+    public VIDEO_PLAYER: string = "2";
+    public SOUND_WIDGET: string = "3";
+    public INTERACTIVE_MAP: string = "4";
+    public TEXT_EDITOR: string = "5";
+            
+}

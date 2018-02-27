a3687ff68b9833008e8983cc509ff0e378c2c27a
Max <maxnakel@gmail.com>
2018-02-26 23:59:27 -0500

    Created Jasmine test for landing-page.

--- src/app/primarySectors/landing-page/landing-page.component.spec.ts
+++ src/app/primarySectors/landing-page/landing-page.component.spec.ts
@@ -1,15 +1,29 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, TestBed, inject, tick, fakeAsync } from '@angular/core/testing';
+import { ToolDescriptionsUS, ToolDescriptionsDE, ToolDescriptionsES } from '../../shared/models/toolDescriptions';
 import { LandingPageComponent } from './landing-page.component';
+import { DebugElement } from '@angular/core';
+import { of } from 'rxjs/observable/of';
 
 describe('LandingPageComponent', () => {
   let component: LandingPageComponent;
   let fixture: ComponentFixture<LandingPageComponent>;
+  //represents HTML debug element
+  let de: DebugElement;
+  //description models
+  let langDescr:ToolDescriptionsUS=new ToolDescriptionsUS();
+
+  //detects if component is created successfully
+  fixture.detectChanges();
+
+  // service = de.injector.get();
+  // let langDescr:ToolDescriptionsUS=new ToolDescriptionsUS();
+  // oversee = spyOn(service, 'explanationProvider').and.returnValue(of(""))
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ LandingPageComponent ]
     })
+    //compiles template and css
     .compileComponents();
   }));
 
@@ -20,6 +34,15 @@ describe('LandingPageComponent', () => {
   });
 
   it('should create', () => {
+    //toBeTruthy() tests whether or not something evaluates to true or false
     expect(component).toBeTruthy();
   });
+
+  // it('should have content Music Player'), ()=>{
+  //   expect(component.content).toContain('Music Player');
+  // }
+
+  // it('should evaluate to FILE_MANAGER', () => {
+  //   expect()
+  // })
 });

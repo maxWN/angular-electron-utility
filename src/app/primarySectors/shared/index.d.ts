04809c47f28225a0202d17c1f7893a4633a4522f
Max <maxnakel@gmail.com>
2018-05-10 23:49:34 -0400

    Added barrel index file and created new shared module.

---
+++ src/app/primarySectors/shared/index.d.ts
@@ -0,0 +1,2 @@
+export * from './components/option-menu/option-menu.component';
+export * from './components/sound-animation/sound-animation.component';
---
+++ src/app/primarySectors/shared/shared.module.ts
@@ -0,0 +1,14 @@
+// import { NgModule } from '@angular/core';
+// import { OptionMenuComponent } from './components/option-menu/option-menu.component';
+// import { SoundAnimationComponent } from './components/sound-animation/sound-animation.component';
+
+// @NgModule({
+//     declarations: [
+//         OptionMenuComponent,
+//         SoundAnimationComponent
+//     ],
+//     imports: [],
+//     providers: [],
+//     bootstrap: []
+//   })
+//   export class SharedModule { }

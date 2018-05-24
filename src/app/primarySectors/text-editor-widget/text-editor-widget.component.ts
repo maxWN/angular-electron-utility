a0a230b9dbaba9e157d983803849ef73d0a9693e
Max <maxnakel@gmail.com>
2018-05-24 00:35:08 -0400

    Added function to strip HTML from plain text editor files in text-editor

--- src/app/primarySectors/text-editor-widget/text-editor-widget.component.ts
+++ src/app/primarySectors/text-editor-widget/text-editor-widget.component.ts
@@ -33,11 +33,18 @@ export class TextEditorWidgetComponent implements OnInit {
     // Save text file 
     public saveFile(): void {
       if (this.text && this.text.length > 0) {
+        // this.text.replace(/<(?:.|\n)*?>/gm, '');
+        this.text = this.createPlainText(this.text);
         let blob = new Blob([this.text], {type: "text/plain;charset=utf-8"});
         // let x_filename = data.headers.get('x-filename');
         saveAs(blob);
       }
     }
+
+    public createPlainText(html): string {
+      let doc = new DOMParser().parseFromString(html, 'text/html');
+      return doc.body.textContent || "";
+   }
 
   // endregion public functions
 

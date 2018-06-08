84eda1b42bb41edcc2ba2e8d4a2bb4158b183b94
Max <maxnakel@gmail.com>
2018-06-07 21:06:05 -0400

    :clipboard: Added fields to modalData.ts.

--- src/app/primarySectors/shared/models/modalData.ts
+++ src/app/primarySectors/shared/models/modalData.ts
@@ -3,10 +3,11 @@ import { Playlist } from './playlist';
     export interface ModalData {
         subTitle: string;
         explanations: Array<string>;
-        currentSongs: Array<Playlist>;
+        currentSongs?: Array<Playlist>;
+        metaData?: MinorDetails;
     }
 
     export interface MinorDetails {
-        metaData: string;
-        loadTime: Date;
+        metaData?: Array<string>;
+        loadTime?: Date;
     }
